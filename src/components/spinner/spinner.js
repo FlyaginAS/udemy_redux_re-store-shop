c5464c697ee28ad6d5a197c0336dd8dcd5b177aa
ruscat <rus.reg.36@gmail.com>
2020-02-19 11:14:46 +0300

    12-11

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import 'spinner.css';
+// import 'spinner.css';
 
 const Spinner=()=>{
     return (
-        <div>Spinner...</div>
+        <div>Loading...</div>
     );
 };
 
