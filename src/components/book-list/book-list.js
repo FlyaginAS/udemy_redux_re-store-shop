b21022bd1a1669aa1350287f62830b5874b16fa9
ruscat <rus.reg.36@gmail.com>
2020-02-23 15:35:23 +0300

    12-16

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -8,7 +8,21 @@ import {compose} from '../../utils';
 import  Spinner from '../spinner';
 import  ErrorIndicator from '../error-indicator';
 
-class BookList extends  Component{
+const BookList = ({books})=>{
+    return (
+        <ul className='book-list'>
+            {
+                books.map((book)=>{
+                    return (
+                        <li key={book.id}><BookListItem book={book}/></li>
+                    )
+                })
+            }
+        </ul>
+    );
+};
+
+class BookListContainer extends  Component{
      componentDidMount(){
         this.props.fetchBooks();
      }
@@ -21,20 +35,12 @@ class BookList extends  Component{
         if(error) {
             return <ErrorIndicator/>;
         }
-        return (
-            <ul className='book-list'>
-                {
-                    books.map((book)=>{
-                        return (
-                            <li key={book.id}><BookListItem book={book}/></li>
-                        )
-                    })
-                }
-            </ul>
-        );
+        return <BookList books={books}/>
     }
 
 }
+
+
 
 const mapStateToProps=(state)=>{
      return {
@@ -53,4 +59,4 @@ const mapStateToProps=(state)=>{
 export  default  compose(
     withBookstoreService(),
     connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
