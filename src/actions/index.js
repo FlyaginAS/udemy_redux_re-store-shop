b384045b50b0bdde61b383c9ca5bc6fbca126c6b
ruscat <rus.reg.36@gmail.com>
2020-02-24 15:07:44 +0300

    12-19

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,7 +31,30 @@ const bookAddedToCart=(bookId)=>{
     }
 };
 
+const bookIncreaseInCart=(bookId)=>{
+   return {
+       type: 'BOOK_ADDED_TO_CART',
+       payload: bookId,
+   }
+};
+
+const bookDecreaseInCart = (bookId)=>{
+   return {
+       type: 'BOOK_DEC_IN_CART',
+       payload: bookId,
+   }
+};
+const bookDelInCart= (bookId)=>{
+    return {
+        type: 'BOOK_DEL_IN_CART',
+        payload: bookId,
+    }
+};
+
 export {
     fetchBooks,
     bookAddedToCart,
+    bookIncreaseInCart,
+    bookDecreaseInCart,
+    bookDelInCart,
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -66,12 +66,49 @@ const reducer = (state = initialState, action)=>{
             const itemIndex=state.cartItems.findIndex(({id})=>id===bookId);
             const item= state.cartItems[itemIndex];
             const newItem=updateCartItem(book, item);
-
-
             return {
                 ...state,
                cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
             };
+        case 'BOOK_DEC_IN_CART':
+            console.log('in reducer dec');
+            const arrCart = state.cartItems;
+            console.log(arrCart);
+            const itemId=action.payload;
+            const itemCart= arrCart.find((item)=>item.id===itemId);
+            let count= itemCart.count;
+            console.log('count is' , count);
+            console.log(itemCart);
+            let newItemCart={
+                ...itemCart,
+                count: itemCart.count - 1,
+                total: itemCart.total - (itemCart.total / itemCart.count)
+            };
+            let newArrCart=arrCart.slice();
+            let itemIdx=arrCart.findIndex((item)=>item.id===itemId);
+            newArrCart.splice(itemIdx, 1, newItemCart);
+            console.log(newArrCart);
+            if(newItemCart.count===0) {
+                newArrCart.splice(itemIdx, 1);
+            }
+            return {
+                ...state,
+                cartItems: newArrCart,
+            };
+        case 'BOOK_DEL_IN_CART':
+            const del=()=>{
+                const arrCart = state.cartItems;
+                const itemId=action.payload;
+                const idx= arrCart.findIndex((item)=>item.id===itemId);
+                let newArrCart=arrCart.slice();
+                newArrCart.splice(idx, 1);
+                return {
+                    ...state,
+                    cartItems: newArrCart,
+                };
+            };
+            return del();
+
         default:
             return state;
     }
